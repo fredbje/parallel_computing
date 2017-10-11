2d2e01cf48f88f0de0e7503a0da96eb0f392e7e6
Fredrik Bjerkås <fbjerkas@gmail.com>
2017-10-11 11:52:37 +0200

    Started on omp version

--- fredbje_ps3/RPS.c
+++ fredbje_ps3/RPS.c
@@ -1,45 +1,79 @@
 #include "RPS.h"
 #include <time.h>
+#include <omp.h>
+#include <math.h>
 
 void swap_petris();
+void init_petri();
 
 cell* petri_A;
 cell* petri_B;
 
+int num_threads, thread_id;
+
 int main(int argc, char** argv){
 
-  printf("running %d iterations\n",ITERATIONS);
+    if(argc==1) {
+        puts("Input number of threads. ex: ./julia 4");
+        return 0;
+    }
+    num_threads = strtod(argv[1], NULL);
+    int temp = sqrt(num_threads);
+    if (num_threads != temp*temp){
+        puts("Number of threads must be a square integer");
+        return 0;
+    }
 
-  srand(time(NULL));
-  petri_A = calloc(IMG_X*IMG_Y, sizeof(cell));
-  petri_B = calloc(IMG_X*IMG_Y, sizeof(cell));
+    printf("running %d iterations..\n",ITERATIONS);
 
-  int seed = rand();
+    srand(time(NULL));
+    petri_A = calloc(IMG_X*IMG_Y, sizeof(cell));
+    petri_B = calloc(IMG_X*IMG_Y, sizeof(cell));
 
-  // Seed some CAs
-  for(int ii = 0; ii < 100; ii++){
-    int rx = rand() % (IMG_X - 1);
-    int ry = rand() % (IMG_Y - 1);
-    int rt = rand() % 4;
+    init_petri(petri_A);
 
-    petri_A[TRANS(rx,ry)].color = rt;
-    petri_A[TRANS(rx,ry)].strength = 1;
-  }
+    #pragma omp parallel private(num_threads, thread_id){
 
-  for(int ii = 0; ii < ITERATIONS; ii++){
+        int bound = IMG_Y / num_threads;
+        int start = thread_id * bound;
+        int finish = start + bound;
+       
 
-    // This should be parallelized somehow
-    iterate_image(petri_A, petri_B);
+       for(int i = 0; i < ITERATIONS; i++){
+            if(i % 100 == 0 && *(thread_id)==0){printf("Progress %d %%\n", i*100 / ITERATIONS);}
+            if(i % 2 == 0){
+                iterate_image(petri_A, petri_B, start, finish);
+            }else{
+                iterate_image(petri_B, petri_A, start, finish);
+            #pragma omp barrier
+        }
+        
+    }
 
-    swap_petris();
-  }
-  make_bmp(petri_A, 0);
+    if(ITERATIONS % 2 == 0){
+        make_bmp(petri_A, 0);
+    } else {
+        make_bmp(petri_B, 0);
+    }
 
+    free(petri_A);
+    free(petri_B);
+    return 0;
+}
+
+void init_petri(cell* petri){
+    for(int ii = 0; ii < 100; ii++){
+        int rx = rand() % (IMG_X - 1);
+        int ry = rand() % (IMG_Y - 1);
+        int rt = rand() % 4;
+
+        petri[TRANS(rx,ry)].color = rt;
+        petri[TRANS(rx,ry)].strength = 1;
+    }
 }
 
 
-void swap_petris(){
-  cell* tmp1 = petri_A;
-  petri_A = petri_B;
-  petri_B = tmp1;
-}
+
+
+
+
